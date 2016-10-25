1b68e9e3f584858537dfc48061862f00ff8d130a
Domi <zlotowska.dominika@gmail.com>
2016-10-25 16:17:46 +0200

    Obiekt1

--- src/com/company/Osoba1.java
+++ src/com/company/Osoba1.java
@@ -3,9 +3,14 @@ package com.company;
 
 public class Osoba1 {
     String imie, nazwisko;
-    int wiek;
+    int wiek, wzrost;
 
     boolean pełnoletnia() {
+
         return wiek >= 18;
+    }
+
+    boolean wysoki() {
+        return wzrost >= 180;
     }
 }
--- src/com/company/Pełnoletnia.java
+++ src/com/company/Pełnoletnia.java
@@ -7,13 +7,15 @@ public class Pełnoletnia {
         tato.imie = "Jan";
         tato.nazwisko = "Kowalski";
         tato.wiek = 35;
+        tato.wzrost = 150;
 
         Osoba1 córka = new Osoba1();
         córka.imie = "Ola";
         córka.nazwisko = "Kowalska";
         córka.wiek = 12;
+        córka.wzrost = 200;
 
-        System.out.println(tato.pełnoletnia());
-        System.out.println(córka.pełnoletnia());
+        System.out.println(tato.pełnoletnia() + " " + tato.wysoki());
+        System.out.println(córka.pełnoletnia() + " " + córka.wysoki());
     }
 }
